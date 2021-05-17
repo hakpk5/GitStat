6a38d2e53122f45c87db9bc775103bbd1603cf2c
pawan <noreply.1824@gmail.com>
2021-05-17 22:32:44 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/Users/Users";
+import axios from "axios";
 class App extends React.Component {
-  state = {};
+  state = {
+    users: [],
+    loading: false,
+  };
+  async componentDidMount() {
+    this.setState({
+      loading: true,
+    });
+    console.log(loading);
+    const response = await axios.get("https://api.github.com/users");
+    console.log(response.data);
+  }
 
   render() {
     return (
